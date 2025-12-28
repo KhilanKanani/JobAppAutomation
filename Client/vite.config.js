9be7f12c29adea237d1091d4a6d964508b1a0086
KhilanKanani <khilankanani50@gmail.com>
2025-12-28 12:37:44 +0530

    JobAi

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -2,21 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwind from "@tailwindcss/vite"
 
-// https://vite.dev/config/
+// vite.config.js
 export default defineConfig({
   plugins: [react(), tailwind()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
-          'redux-vendor': ['@reduxjs/toolkit', 'react-redux', 'redux'],
-        },
-      },
-    },
-    chunkSizeWarningLimit: 1000,
-  },
-  optimizeDeps: {
-    include: ['react', 'react-dom', 'react-router-dom'],
-  },
-})
+});
